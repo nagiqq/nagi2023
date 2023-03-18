a30b38b81004ea1ac93d590c3205a9d58e0b0eb3
nagiqq <nagiichinichi@gmail.com>
2023-03-18 19:27:21 +0800

    [ Update ]: ico

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,21 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
+  // app
+  app: {
+    head: {
+      title: 'nagiliu | blog',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'nagi liu | blog'
+        }
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/myicon.ico' }]
+    }
+  },
   css: [
     'virtual:windi-base.css',
     'virtual:windi-components.css',
